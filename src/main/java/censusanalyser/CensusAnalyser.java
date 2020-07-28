7f1823db8ad2b12e191104d55cddf94093e5fd86
sonvin2916 <sonalchougule07@gmail.com>
2020-07-28 17:59:39 +0530

    [Sonal]Added : Census Analyser to load the Indian States Census Information file

---
+++ src/main/java/censusanalyser/CensusAnalyser.java
@@ -0,0 +1,32 @@
+package censusanalyser;
+
+
+
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Iterator;
+import java.util.stream.StreamSupport;
+
+public class CensusAnalyser {
+    public int loadIndiaCensusData(String csvFilePath) throws IOException
+    {
+            Reader reader = Files.newBufferedReader(Paths.get(csvFilePath));
+            CsvToBeanBuilder<IndiaCensusCSV> csvToBeanBuilder = new CsvToBeanBuilder<>(reader);
+            csvToBeanBuilder.withType(IndiaCensusCSV.class);
+            csvToBeanBuilder.withIgnoreLeadingWhiteSpace(true);
+            CsvToBean<IndiaCensusCSV> csvToBean = csvToBeanBuilder.build();
+            Iterator<IndiaCensusCSV> censusCSVIterator = csvToBean.iterator();;
+            int numOfEntries = 0;
+            while (censusCSVIterator.hasNext()) {
+                numOfEntries++;
+                IndiaCensusCSV censusData = censusCSVIterator.next();
+            }
+            return numOfEntries;
+
+    }
+}
