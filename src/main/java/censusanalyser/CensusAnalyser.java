0ae15faa680017e283ecd88f6514e04bd84ebb7f
sonvin2916 <sonalchougule07@gmail.com>
2020-07-28 19:00:02 +0530

    [Sonal]Added : When wrong CSV file passed returns a custom exception

--- src/main/java/censusanalyser/CensusAnalyser.java
+++ src/main/java/censusanalyser/CensusAnalyser.java
@@ -13,19 +13,23 @@ import java.util.Iterator;
 import java.util.stream.StreamSupport;
 
 public class CensusAnalyser {
-    public int loadIndiaCensusData(String csvFilePath) throws IOException
-    {
+    public int loadIndiaCensusData(String csvFilePath) throws  CensusAnalyserException {
+        try
+        {
             Reader reader = Files.newBufferedReader(Paths.get(csvFilePath));
             CsvToBeanBuilder<IndiaCensusCSV> csvToBeanBuilder = new CsvToBeanBuilder<>(reader);
             csvToBeanBuilder.withType(IndiaCensusCSV.class);
             csvToBeanBuilder.withIgnoreLeadingWhiteSpace(true);
             CsvToBean<IndiaCensusCSV> csvToBean = csvToBeanBuilder.build();
             Iterator<IndiaCensusCSV> censusCSVIterator = csvToBean.iterator();;
-            int numOfEntries = 0;
-            while (censusCSVIterator.hasNext()) {
-                numOfEntries++;
-                IndiaCensusCSV censusData = censusCSVIterator.next();
-            }
+            Iterable<IndiaCensusCSV> censusCSVIterable=() -> censusCSVIterator;
+            int numOfEntries = (int) StreamSupport.stream(censusCSVIterable.spliterator(),false).count();
             return numOfEntries;
+        }
+        catch (IOException e)
+        {
+            throw new CensusAnalyserException(e.getMessage(),CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM);
+        }
+
     }
 }
