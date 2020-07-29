c1476d6bd787b2814a5e296064fd628862178205
sonvin2916 <sonalchougule07@gmail.com>
2020-07-29 20:23:37 +0530

    [Sonal]Refactored : Inroduced CSV Builder for Programming Interface to address open close principal

--- src/main/java/censusanalyser/OpenCSVBuilder.java
+++ src/main/java/censusanalyser/OpenCSVBuilder.java
@@ -6,9 +6,9 @@ import com.opencsv.bean.CsvToBeanBuilder;
 import java.io.Reader;
 import java.util.Iterator;
 
-public class OpenCSVBuilder {
+public class OpenCSVBuilder <E> implements ICSVBuilder{
 
-    public<E> Iterator<E> getCSVFileIterator(Reader reader, Class<E> csvClass) throws CensusAnalyserException {
+    public Iterator<E> getCSVFileIterator(Reader reader, Class csvClass) throws CensusAnalyserException {
         try
         {
             CsvToBeanBuilder<E> csvToBeanBuilder = new CsvToBeanBuilder<>(reader);
